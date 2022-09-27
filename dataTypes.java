2e0775657f9cc451578f2b9dee456c7fa3159603
Şeyda Gü Topal <92445525+seydagultopal@users.noreply.github.com>
2022-09-27 17:27:34 +0300

    Add files via upload

---
+++ dataTypes.java
@@ -0,0 +1,26 @@
+package javaProject;
+
+public class dataTypes {
+
+	public static void main(String[] args) {
+		int number=258;
+		number=159;
+		
+		double num=25.7;
+		num=25;
+		
+		char character= 'A';
+		character='B';
+		
+		boolean right = true;//or false
+		
+		System.out.println(number);
+		System.out.println(num);
+		System.out.println(character);
+		
+		if(right==true) {}
+		System.out.println("True");
+
+	}
+
+}
