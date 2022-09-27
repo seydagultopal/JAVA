909e4151bfe258bc6f32dbb502d1b1dd50ac47c0
Şeyda Gü Topal <92445525+seydagultopal@users.noreply.github.com>
2022-09-27 17:29:42 +0300

    Add files via upload

---
+++ findANumber.java
@@ -0,0 +1,27 @@
+package javaProject;
+
+public class findANumber {
+
+	public static void main(String[] args) {
+		int[] numbers =new int[] { 1,2,3,4,5,6,7,8,9};
+		int wanted=10;
+        boolean isTrue= false;
+        
+        for (int number : numbers) {
+        	if(number == wanted) {
+        		isTrue=true;
+        		break;
+        	}
+        }
+        
+        if(isTrue) {
+        	System.out.println("number is avaible");
+        }
+        else {
+        	System.out.println("number is not avaible");
+        }
+        
+
+	}
+
+}
