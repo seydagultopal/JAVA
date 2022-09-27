027168b1c4036bbf1f068581a0cf476e3826b18c
Şeyda Gü Topal <92445525+seydagultopal@users.noreply.github.com>
2022-09-27 17:52:34 +0300

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,15 @@
+package javaProject;
+
+public class variables {
+
+	public static void main(String[] args) {
+		int ogrenciSayisi = 12;
+        String mesaj = "Öğrenci Sayısı : ";
+	    System.out.println(mesaj + ogrenciSayisi);
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println(mesaj + ogrenciSayisi);
+
+	}
+
+}
