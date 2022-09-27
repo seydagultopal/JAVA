cbb0a9f4d95a9896d0a05051ca5e9b080340fcda
Şeyda Gü Topal <92445525+seydagultopal@users.noreply.github.com>
2022-09-27 17:25:39 +0300

    Add files via upload

---
+++ conditionals.java
@@ -0,0 +1,21 @@
+package javaProject;
+
+public class conditionals {
+
+	public static void main(String[] args) {
+		
+			int number= 5;
+			
+			if(number>20) {
+				System.out.println("the number is greater than 20");
+			}
+			else if(number==20) {
+				System.out.println("the number is equal to 20");
+			}
+			else {
+				System.out.println("the number is less than 20");
+			}
+
+	}
+
+}
