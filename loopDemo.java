791aae38bfa8bebf6a290c6784e3b46338d87d02
Şeyda Gü Topal <92445525+seydagultopal@users.noreply.github.com>
2022-09-27 17:39:39 +0300

    Add files via upload

---
+++ loopDemo.java
@@ -0,0 +1,33 @@
+package javaProject;
+
+public class loopDemo {
+
+	public static void main(String[] args) {
+		//for
+				for(int i=1;i<10;i++) {
+				System.out.println(i);
+				}
+				System.out.println("For loop is over");
+				
+				
+				//while
+				
+				int i=1;
+				while(i<10) {
+					System.out.println(i);
+					i+=3;
+				}
+				System.out.println("while loop is over");
+				
+				//do-while
+				int j=3;
+				do {
+					System.out.println(j);
+					j+=4;
+				}
+				while(j<20);
+					System.out.println("do*while loop is over");
+
+	}
+
+}
